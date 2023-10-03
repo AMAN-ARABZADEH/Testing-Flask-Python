dd6b16a4d7f18c87675dd53f079058cb6c9dbc72
Aman Arabzadeh <aman.arabzadeh98@gmail.com>
2023-10-03 09:58:11 +0200

    Testing Flask

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from cs50 import SQL
 
 app = Flask(__name__)
 
-REGISTRANTS = {}
+# Establish a connection to the SQLite database
+db = SQL("sqlite:///registrants.db")
+
+# Predefined list of sports
 SPORTS = ["Basketball", "Soccer", "Baseball", "Tennis", "Cricket", "Rugby", "Golf", "Swimming", "Cycling", "Badminton"]
 
-
 @app.route("/")
-
 def index():
     return render_template("index.html", sports=SPORTS)
 
 @app.route('/register', methods=["POST"])
 def register():
     name = request.form.get("name")
-    if not name:
-        return render_template("failure.html")
     sport = request.form.get("sport")
-    if sport not in SPORTS:
+
+    # Validation
+    if not name or sport not in SPORTS:
         return render_template("failure.html")
-    REGISTRANTS[name] = sport
-    return render_template("register.html", name = name, sport = sport)
+
+    # Insert into the database
+    db.execute("INSERT INTO registrants (name, sport) VALUES (?, ?)", name, sport)
+
+    # Return the register confirmation page
+    return render_template("register.html", name=name, sport=sport)
 
 @app.route("/registrants")
 def registrants():
-    return render_template("registrants.html", REGISTRANTS=REGISTRANTS)
-
+    rows = db.execute("SELECT name, sport FROM registrants")
+    return render_template("registrants.html", registrants=rows)
 
 if __name__ == '__main__':
     app.run(debug=True)
